f74766a2f333c89b949379b0d36687d0aef75acb
alectramell <alectramell@gmail.com>
2017-07-19 19:17:47 -0700

    spiders

--- extract.sh
+++ extract.sh
@@ -2,22 +2,18 @@
 
 clear
 
-echo "Time Stamp: " > /home/$(whoami)/Desktop/spiderbyte.txt
+echo "TIMESTAMP: " > /home/$(whoami)/Desktop/spiderbyte.txt
 date -R >> /home/$(whoami)/Desktop/spiderbyte.txt 
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt 
-echo "IP Info:" >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo "IP DATA:" >> /home/$(whoami)/Desktop/spiderbyte.txt
 curl ipinfo.io >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo "Hostname:" >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo "HOSTNAME:" >> /home/$(whoami)/Desktop/spiderbyte.txt
 hostname -I >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo "History Log:" >> /home/$(whoami)/Desktop/spiderbyte.txt
-echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
-history >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo "CURRENT NETWORK:" >> /home/$(whoami)/Desktop/spiderbyte.txt
+iwgetid -s >> /home/$(whoami)/Desktop/spiderbyte.txt
 echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
 
 clear
 
-echo "..Press [ANY KEY] to END Spider.."
+gnome-open /home/$(whoami)/Desktop/spiderbyte.txt &
+
+clear
