fc84167b891e0ccf530f2e45506f396d23a9a7d2
alectramell <alectramell@gmail.com>
2017-07-19 18:33:28 -0700

    spiders

--- extract.sh
+++ extract.sh
@@ -2,4 +2,22 @@
 
 clear
 
-curl ipinfo.io
+echo "Time Stamp: " > /home/$(whoami)/Desktop/spiderbyte.txt
+date -R >> /home/$(whoami)/Desktop/spiderbyte.txt 
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt 
+echo "IP Info:" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+curl ipinfo.io >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo "Hostname:" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+hostname -I >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo "History Log:" >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+history >> /home/$(whoami)/Desktop/spiderbyte.txt
+echo -e "\n" >> /home/$(whoami)/Desktop/spiderbyte.txt
+
+clear
+
+echo "..Press [ANY KEY] to END Spider.."
